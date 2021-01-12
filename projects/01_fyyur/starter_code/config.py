a69f77d7936d798a62a685bfdbcd6e02b83aa1d0
robinloujun <jun.lou.chn@outlook.com>
2021-01-12 17:02:45 +0100

    remove todo tag for finished work

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -8,7 +8,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-# TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/fyyur"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
