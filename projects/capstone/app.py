faf722794f650f97f8545f13e436d3c51738261b
Jun Lou <jun.lou.chn@outlook.com>
2021-02-18 23:38:14 +0100

    adapt the basic route and add error handling

--- projects/capstone/app.py
+++ projects/capstone/app.py
@@ -2,24 +2,60 @@ import os
 from flask import Flask
 from models import setup_db
 
+
 def create_app(test_config=None):
 
     app = Flask(__name__)
     setup_db(app)
-    CORS(app)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     @app.route('/')
-    def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello" 
-        if excited == 'true': greeting = greeting + "!!!!!"
-        return greeting
+    def index():
+        return "Welcome to the Capstone Car Booking Platform!"
 
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
+    # Error Handling
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'bad request',
+        }), 400
+
+    @app.errorhandler(401)
+    def method_not_allowed(error):
+        return jsonify({
+            'success': False,
+            'error': 401,
+            'message': 'method not allowed',
+        }), 401
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'resource not found',
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'internal server error',
+        }), 500
 
     return app
+
 
 app = create_app()
 
