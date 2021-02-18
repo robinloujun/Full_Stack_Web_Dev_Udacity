ab1ef909af1889549f771ef2b0921846fdb71f47
Jun Lou <jun.lou.chn@outlook.com>
2021-02-18 23:43:11 +0100

    set up error handling in api

--- projects/capstone/app.py
+++ projects/capstone/app.py
@@ -13,47 +13,6 @@ def create_app(test_config=None):
     def index():
         return "Welcome to the Capstone Car Booking Platform!"
 
-    # Error Handling
-    @app.errorhandler(400)
-    def bad_request(error):
-        return jsonify({
-            'success': False,
-            'error': 400,
-            'message': 'bad request',
-        }), 400
-
-    @app.errorhandler(401)
-    def method_not_allowed(error):
-        return jsonify({
-            'success': False,
-            'error': 401,
-            'message': 'method not allowed',
-        }), 401
-
-    @app.errorhandler(404)
-    def not_found(error):
-        return jsonify({
-            'success': False,
-            'error': 404,
-            'message': 'resource not found',
-        }), 404
-
-    @app.errorhandler(422)
-    def unprocessable(error):
-        return jsonify({
-            "success": False,
-            "error": 422,
-            "message": "unprocessable"
-        }), 422
-
-    @app.errorhandler(500)
-    def internal_server_error(error):
-        return jsonify({
-            'success': False,
-            'error': 500,
-            'message': 'internal server error',
-        }), 500
-
     return app
 
 
---
+++ projects/capstone/app/api/errors.py
@@ -0,0 +1,47 @@
+from flask import jsonify
+from . import api
+
+# Error Handling
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        'success': False,
+        'error': 400,
+        'message': 'bad request',
+    }), 400
+
+
+@app.errorhandler(401)
+def method_not_allowed(error):
+    return jsonify({
+        'success': False,
+        'error': 401,
+        'message': 'method not allowed',
+    }), 401
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'resource not found',
+    }), 404
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        'success': False,
+        'error': 500,
+        'message': 'internal server error',
+    }), 500
