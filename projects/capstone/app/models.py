f128c683eca499b2d947bfa45f2fcbbc4c0068e9
Jun Lou <jun.lou.chn@outlook.com>
2021-02-19 21:24:16 +0100

    add to_json and repr

--- projects/capstone/app/models.py
+++ projects/capstone/app/models.py
@@ -1,16 +1,13 @@
 import json
-from markdown import markdown
 from datetime import datetime
 from . import db
-from flask_login import UserMixin, AnonymousUserMixin
-
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
 
 
 def setup_db(app, database_path=database_path):
+    '''
+    setup_db(app)
+        binds a flask application and a SQLAlchemy service
+    '''
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
@@ -30,6 +27,20 @@ class Vehicle(db.Model):
     automatic = db.Column(db.Boolean)
     bookings = db.relationship("Booking", backref="vehicles")
 
+    def to_json(self):
+        return {
+            "VIN": self.VIN,
+            "make": self.make,
+            "model": self.model,
+            "model_year": self.model_year,
+            "fuel_type": self.fuel_type,
+            "standard_seat_number": self.standard_seat_number,
+            "automatic": self.automatic,
+        }
+
+    def __repr__(self):
+        return json.dumps(self.to_json())
+
 
 class Client(db.Model):
     __tablename__ = "clients"
@@ -39,6 +50,18 @@ class Client(db.Model):
     surname = db.Column(db.String(64))
     email = db.Column(db.String(64), unique=True, index=True)
     bookings = db.relationship("Booking", backref="clients")
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "client_uuid": self.client_uuid,
+            "forename": self.forename,
+            "surname": self.surname,
+            "email": self.email,
+        }
+
+    def __repr__(self):
+        return json.dumps(self.to_json())
 
 
 class Booking(db.Model):
@@ -51,3 +74,15 @@ class Booking(db.Model):
     end_datetime = db.Column(db.DateTime, nullable=False)
     vehicle = db.relationship("Vehicle")
     client = db.relationship("Client")
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "vehicle_VIN": self.vehicle_VIN,
+            "client_id": self.client_id,
+            "start_datetime": self.start_datetime,
+            "end_datetime": self.end_datetime,
+        }
+
+    def __repr__(self):
+        return json.dumps(self.to_json())
