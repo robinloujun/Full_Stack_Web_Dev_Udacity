324739479b3a293074fd2364b6b6df4f653eb75b
Jun Lou <jun.lou.chn@outlook.com>
2021-02-22 22:51:21 +0100

    fix typo

--- projects/capstone/app/__init__.py
+++ projects/capstone/app/__init__.py
@@ -17,6 +17,6 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     from .api import api as api_blueprint
-    app.register_blueprint(api_blueprint. url_prefix="/api/v1")
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     return app
