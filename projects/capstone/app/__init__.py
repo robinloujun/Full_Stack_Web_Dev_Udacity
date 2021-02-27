14945ae9612971d5bb86880d6576fc6a0ee379eb
Jun Lou <jun.lou.chn@outlook.com>
2021-02-28 00:13:31 +0100

    add basic route

--- projects/capstone/app/__init__.py
+++ projects/capstone/app/__init__.py
@@ -19,4 +19,8 @@ def create_app(config_name):
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
+    @app.route('/')
+    def index():
+        return 'Hello, Capstone'
+
     return app
