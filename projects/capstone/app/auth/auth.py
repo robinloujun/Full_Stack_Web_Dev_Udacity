cba3adf355a8b2b4dcf5d7f5099c92d1d8b4cf78
Jun Lou <jun.lou.chn@outlook.com>
2021-02-28 16:08:16 +0100

    adapt env var

--- projects/capstone/app/auth/auth.py
+++ projects/capstone/app/auth/auth.py
@@ -4,9 +4,9 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-AUTH0_DOMAIN = 'username.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'http://localhost:5000'
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+ALGORITHMS = os.environ['ALGORITHMS']
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
 
 class AuthError(Exception):
